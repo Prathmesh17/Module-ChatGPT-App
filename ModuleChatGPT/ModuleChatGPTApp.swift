da7949fed1ceab376bd2f387fc7dc3f02fb6eb26
Prathmesh <prathmeshparteki@gmail.com>
2023-12-21 18:48:45 +0530

    Initial Commit

---
+++ ModuleChatGPT/ModuleChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModuleChatGPTApp.swift
+//  ModuleChatGPT
+//
+//  Created by Prathmesh Parteki on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ModuleChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
